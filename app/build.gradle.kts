2c0fd742de0c2fb91915b2ea36367974ab61b756
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-03-09 18:33:08 -0300

    Define Automatic-Module-Name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,3 +10,7 @@ dependencies {
     implementation(project(":api"))
     runtimeOnly(project(":impl"))
 }
+
+tasks.named<Jar>("jar").configure {
+    manifest.attributes["Automatic-Module-Name"] = "br.dev.pedrolamarao.app"
+}
